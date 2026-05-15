2a1a9506de9829d4433f843eb11a780153166f07
candyh98 <candyh9898@gmail.com>
2026-05-15 16:38:57 +0800

    step 5 : update listings template

--- listings/models.py
+++ listings/models.py
@@ -3,7 +3,7 @@ from doctors.models import Doctor
 
 # Create your models here.
 class Listing(models.Model):
-    doctors=models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+    doctor=models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
     title=models.CharField(max_length=200)
     address=models.CharField(max_length=200)
     district=models.CharField(max_length=50)
@@ -13,11 +13,11 @@ class Listing(models.Model):
     services=models.IntegerField()
     room_type=models.CharField(max_length=50)
     rooms=models.IntegerField()
-    photos_main=models.ImageField(upload_to='photo/%Y/%m/%d')
-    photos_1=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
-    photos_2=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
-    photos_3=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
-    photos_4=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
+    photo_main=models.ImageField(upload_to='photo/%Y/%m/%d')
+    photo_1=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
+    photo_2=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
+    photo_3=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
+    photo_4=models.ImageField(upload_to='photo/%Y/%m/%d',blank=True)
     is_published=models.BooleanField(default=True)
     list_date=models.DateTimeField(auto_now_add=True)
 
